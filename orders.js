e924461286f945d63446d97b9a5a84153a9c38f4
matheus <matheusxavi57@gmail.com>
2023-11-23 01:55:59 +0000

    Estrutura do Hub

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Dipirona ',
+        productNumber: '3297',
+        paymentStatus: 'Due',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Alivium',
+        productNumber: '8140',
+        paymentStatus: 'Devolvido',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Torsilax',
+        productNumber: '6449',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
