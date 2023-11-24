77d51c38baef5dc825c061220b0543bfa7ee5046
matheus <matheusxavi57@gmail.com>
2023-11-24 01:54:58 +0000

    talvez o ultimo commit

---
+++ historico/historico.js
@@ -0,0 +1,21 @@
+const Orders = [
+    {
+        productName: 'Dipirona',
+        productNumber: '7823',
+        paymentStatus: '23/11',
+        status: 'Pedido separado'
+    },
+    {
+        productName: 'Alivium',
+        productNumber: '1450',
+        paymentStatus: '21/11',
+        status: 'Saiu para entrega'
+    },
+    {
+        productName: 'Torsilax',
+        productNumber: '0649',
+        paymentStatus: '19/11',
+        status: 'Saiu para entrega'
+    },
+    
+]
---
+++ historico/tudo.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Dipirona',
+        productNumber: '7823',
+        paymentStatus: '23/11',
+        status: 'Pedido separado'
+    },
+    {
+        productName: 'Alivium',
+        productNumber: '1450',
+        paymentStatus: '21/11',
+        status: 'Saiu para entrega'
+    },
+    {
+        productName: 'Torsilax',
+        productNumber: '0649',
+        paymentStatus: '19/11',
+        status: 'Saiu para entrega'
+    },
+    {
+        productName: 'Ozempic',
+        productNumber: '4397',
+        paymentStatus: '27/10',
+        status: 'Entregue'
+    },
+    {
+        productName: 'Venvanse',
+        productNumber: '1840',
+        paymentStatus: '24/10',
+        status: 'Entregue'
+    },
+    {
+        productName: 'Minoxidil',
+        productNumber: '3259',
+        paymentStatus: '16/10',
+        status: 'Entregue'
+    },
+]
